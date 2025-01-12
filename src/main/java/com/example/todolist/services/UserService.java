54c0095ea1b6e38c496fabeff8e9798741c3ba5f
VelentynKoval <J.K.play2015@gmail.com>
2025-01-12 15:56:57 +0200

    create user service with method to create new user

---
+++ src/main/java/com/example/todolist/services/UserService.java
@@ -0,0 +1,33 @@
+package com.example.todolist.services;
+
+import com.example.todolist.dto.UserDto;
+import com.example.todolist.mappers.UserMapper;
+import com.example.todolist.models.User;
+import com.example.todolist.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), user.getAuthorities()
+        );
+    }
+
+    public void createNewUser(UserDto userDto) {
+        User user = userMapper.toEntity(userDto);
+        user.setActive(true);
+        userRepository.save(user);
+    }
+}
