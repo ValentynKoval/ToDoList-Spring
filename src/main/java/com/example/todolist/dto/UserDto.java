5e493df2d331aa8e8ff64b7d4177acfc9980e6a4
VelentynKoval <J.K.play2015@gmail.com>
2025-01-12 15:56:19 +0200

    create user dto for registration

--- src/main/java/com/example/todolist/dto/UserDto.java
+++ src/main/java/com/example/todolist/dto/UserDto.java
@@ -1,9 +1,13 @@
 package com.example.todolist.dto;
 
+import com.example.todolist.models.Role;
 import lombok.Data;
 
 @Data
 public class UserDto {
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
+    private Role role;
 }
