8ac2d34c8b07b785fc377ecf35053163daef7e1b
VelentynKoval <J.K.play2015@gmail.com>
2025-01-12 16:22:17 +0200

    create token model

---
+++ src/main/java/com/example/todolist/models/Token.java
@@ -0,0 +1,24 @@
+package com.example.todolist.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    public boolean revoked = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
