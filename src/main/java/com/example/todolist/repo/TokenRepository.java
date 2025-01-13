bad5ec158190ff75a856a4281fd9627ee8db00fb
VelentynKoval <J.K.play2015@gmail.com>
2025-01-13 19:11:40 +0200

    create by token method

--- src/main/java/com/example/todolist/repo/TokenRepository.java
+++ src/main/java/com/example/todolist/repo/TokenRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByToken(String token);
+    void deleteByToken(String token);
 }
