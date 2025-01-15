470e0a9dea5f41a0d4056a4c411b44e1257e5266
VelentynKoval <J.K.play2015@gmail.com>
2025-01-15 14:24:17 +0200

    adding the function of automatic deletion of expired tokens

--- src/main/java/com/example/todolist/repo/TokenRepository.java
+++ src/main/java/com/example/todolist/repo/TokenRepository.java
@@ -1,7 +1,10 @@
 package com.example.todolist.repo;
 
 import com.example.todolist.models.Token;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,9 @@ import java.util.Optional;
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByToken(String token);
     void deleteByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Token t WHERE t.expired = true")
+    int deleteAllByExpiredTrue();
 }
